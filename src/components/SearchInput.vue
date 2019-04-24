9df8f181f5221b7f943ae1566ccd56a16807796d
Rauven <jstepien50@gmail.com>
2019-04-24 18:23:07 +0200

    Add basic hero image view

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="searchWrapper">
+      <input
+        id="search"
+        name="search"
+        v-model="searchValue"
+        @input="handleInput"
+      />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchInput'
+}
+</script>
+
+<style lang="scss" scoped>
+    .searchWrapper {
+        width: 250px;
+        display: flex;
+        flex-direction: column;
+
+        input {
+            height: 30px;
+            border: 0;
+            background: none;
+            border-bottom: 1px solid black;
+        }
+    }
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,38 +1,32 @@
 <template>
   <div class="wrapper">
-    <div class="search">
-      <label for="search">Search</label>
-      <input 
-        id="search" 
-        name="search" 
-        v-model="searchValue"
-        @input="handleInput" 
-      />
-      <ul>
-        <li v-for="item in results" :key="item.data[0].nasa_id">
-          <p>{{ item.data[0].description }}</p>
-        </li>
-      </ul>
-    </div>
+    <Claim />
+    <SearchInput />
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import debounce from 'lodash.debounce';
+import Claim from '@/components/Claim.vue';
+import SearchInput from '@/components/SearchInput.vue';
 
 const API = 'https://images-api.nasa.gov/search';
 
 export default {
   name: 'Search',
+  components: {
+    Claim,
+    SearchInput,
+  },
   data() {
     return {
       searchValue: '',
-      results: []
+      results: [],
     };
   },
   methods: {
-    handleInput: debounce(function() {
+    handleInput: debounce(function () {
       axios.get(`${API}?q=${this.searchValue}&media_type=image`)
         .then((response) => {
           this.results = response.data.collection.items;
@@ -40,34 +34,24 @@ export default {
         .catch((error) => {
           console.log(error);
         });
-    }, 500)
-  }
+    }, 500),
+  },
 };
 </script>
 
 <style lang="scss" scoped>
   .wrapper {
     width: 100%;
+    height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     margin: 0;
     padding: 30px;
-  }
-
-  .search {
-    width: 300px;
-    display: flex;
-    flex-direction: column;
-
-    label {
-      font-family: Montserrat, sans-serif;
-    }
-
-    input {
-      height: 30px;
-      border: 0;
-      border-bottom: 1px solid black;
-    }
+    background-image: url('../assets/heroimage.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 80% 0;
   }
 </style>
